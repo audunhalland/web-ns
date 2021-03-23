a7d52cddb27915942be443689ae5a56ab41ca2a3
Audun Halland <audun.halland@gmail.com>
2021-03-23 23:07:27 +0100

    refactor: restructure modules + implement traits for InternalAttr

--- src/internal.rs
+++ src/internal.rs
@@ -1,5 +1,31 @@
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+use crate::attribute_format::AttributeFormat;
+
 pub struct InternalAttr {
     pub attribute: &'static str,
     pub property: &'static str,
+}
+
+impl PartialEq<InternalAttr> for InternalAttr {
+    fn eq(&self, other: &Self) -> bool {
+        self.attribute == other.attribute
+    }
+}
+impl Eq for InternalAttr {}
+
+impl std::hash::Hash for InternalAttr {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.attribute.hash(state);
+    }
+}
+
+impl PartialOrd for InternalAttr {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.attribute.partial_cmp(other.attribute)
+    }
+}
+
+impl Ord for InternalAttr {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.attribute.cmp(other.attribute)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,16 @@
 
+pub mod attribute;
+
+mod attribute_format;
 mod macros;
 mod internal;
 
-mod html;
+mod schema {
+    pub mod html;
+}
 
 pub enum Schema {
     Html5
-}
-
-pub enum AttributeValue {
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct Attribute(AttrAlloc);
-
-///
-/// A web attribute.
-///
-impl Attribute {
-    ///
-    /// Access the markup attribute name of this attribute.
-    ///
-    pub fn attribute(&self) -> &str {
-        match &self.0 {
-            AttrAlloc::Internal(attr) => attr.attribute,
-            AttrAlloc::Data(attr) => &attr.strbuf[..attr.buf_property_start],
-        }
-    }
-
-    ///
-    /// Access the JS property name of this attribute.
-    ///
-    pub fn property(&self) -> &str {
-        match &self.0 {
-            AttrAlloc::Internal(attr) => attr.property,
-            AttrAlloc::Data(attr) => &attr.strbuf[attr.buf_property_start..],
-        }
-    }
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
-enum AttrAlloc {
-    Internal(&'static internal::InternalAttr),
-    Data(Box<DataAttr>),
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
-struct DataAttr {
-    strbuf: String,
-    buf_property_start: usize,
-}
-
-pub fn parse_attribute(attribute: &str, schema: Schema) -> Option<Attribute> {
-    match html::attrs::internal_attr_by_name(attribute) {
-        Some(internal_attr) => Some(Attribute(AttrAlloc::Internal(internal_attr))),
-        None => None
-    }
-}
-
-pub fn parse_property(property: &str, schema: Schema) -> Option<Attribute> {
-    match html::attrs::internal_attr_by_property(property) {
-        Some(internal_attr) => Some(Attribute(AttrAlloc::Internal(internal_attr))),
-        None => None
-    }
 }
 
 #[cfg(test)]

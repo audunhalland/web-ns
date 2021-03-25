d07a718562aa491a9db33392fd90e42a71c9fc7c
Audun Halland <audun.halland@gmail.com>
2021-03-25 14:23:09 +0100

    chore: update safety comments

--- src/phf_util.rs
+++ src/phf_util.rs
@@ -1,8 +1,9 @@
 use unicase::UniCase;
 
 pub fn phf_str_lookup(map: &phf::Map<&'static str, usize>, key: &str) -> Option<usize> {
-    // safety: It is safe to temporarily treat the key as 'static within this function,
-    // the map is immutable, and cannot store a reference to this key.
+    // safety: Given that `phf` does not allocate this should be safe.
+    // It would need to use a global Mutex and leak this reference into a static, since `map` is immutable.
+    // phf would never do that.
     let key: &'static str = unsafe { std::mem::transmute(key) };
     map.get(&key).map(|index| *index)
 }
@@ -11,8 +12,9 @@ pub fn phf_unicase_lookup(
     map: &phf::Map<UniCase<&'static str>, usize>,
     key: &str,
 ) -> Option<usize> {
-    // safety: It is safe to temporarily treat the key as 'static within this function,
-    // the map is immutable, and cannot store a reference to this key.
+    // safety: Given that `phf` does not allocate this should be safe.
+    // It would need to use a global Mutex and leak this reference into a static, since `map` is immutable.
+    // phf would never do that.
     let key: &'static str = unsafe { std::mem::transmute(key) };
     map.get(&UniCase::new(key)).map(|index| *index)
 }
